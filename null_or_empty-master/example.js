dba7ae601f9d82bbdf87a42a72c7e22e99f394cf
hasanmd91 <hasanmd1991@hotmail.com>
2023-01-16 12:56:40 +0200

    clg

--- null_or_empty-master/example.js
+++ null_or_empty-master/example.js
@@ -1,8 +1,8 @@
 // Change './index' to 'is-null-or-empty' if you use this code outside of this package
-var isNullOrEmpty = require('./index');
+var isNullOrEmpty = require("./index");
 
 console.log(isNullOrEmpty("")); // true
 console.log(isNullOrEmpty(null)); // true
 console.log(isNullOrEmpty(undefined)); // true
 
-console.log(isNullOrEmpty("Hello World")); // false
+console.log(isNullOrEmpty("Hello our World")); // false
